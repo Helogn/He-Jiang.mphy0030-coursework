683e6bec6b49545542952038464c27f2b7d96571
Helogn <58104248+Helogn@users.noreply.github.com>
2021-12-07 20:21:38 +0000

    Risk1
    Almost finish

---
+++ cw1/task1/task.py
@@ -0,0 +1,116 @@
+# HE JIANG
+# 2021-12-2
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+
+# Task 1 Distance Transform in NumPy 
+# • Implement a function distance_transform_np, which takes a 3D volumetric binary image as input and 
+# returnsits 3D Euclidean distance transform. The function should accept the second argument as the 
+# voxel dimensions in each axis and the computed distance transform should be in the unit of 
+# millimetre. You should use onlyNumPy for implementing this function. [6]
+
+# • Briefly describe the algorithm you used in the function docstring.[3]
+# • Compare the built-in function distance_transform_edt in scipy.ndimage with your implementation.
+# Implement a task script “task.py”, under folder “task1”, performing the following:
+# o Download the “label_train00.npy” file, and use numpy.load to load. [1]
+# o Compute distance transform of the segmentation boundary using the two implementations, 
+# i.e. distance_transform_np and distance_transform_edt.[4]
+# o Time the speed of two implementations, and comment on the difference.[3]
+# o Compute the mean and standard deviation of the voxel-level difference between the two 
+# implementations, and comment on the difference.[3]
+# o Save 5 example slices to PNG files (filename being slice index), across the volume, together 
+# with their corresponding distance transform results for each of the two algorithms.[5]
+
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+
+
+import numpy as np
+import matplotlib.pyplot  as plt
+# def convolutional_matrix():
+#     np.convolve(output_image)
+#     kernal = []
+#     if x,y,z
+
+def distance_transform_np( input_image , Dis = [1,1,1] ):
+    # print(input_image.shape)
+    # print(np.size(input_image))
+    Aim = np.sum(input_image)
+    print(" Aim = " + str(Aim))
+    input_dim = input_image.shape
+    min = np.amin(input_image) 
+    max = np.amax(input_image)
+
+
+    # expand output_image 2 voxels by each coordinate 
+    output_image = np.zeros([input_dim[0]+2,input_dim[1]+2,input_dim[2]+2])
+    output_image[1:input_dim[0]+1,1:input_dim[1]+1,1:input_dim[2]+1] = input_image
+    output_image1 = output_image
+    output_image2 = output_image
+    
+    # design matrix
+    Ori = 1
+    while True :
+        Jug1 = 1
+        Jug2 = 1
+        for x in range(input_dim[0]):
+            for y in range(input_dim[1]):
+                for z in range(input_dim[2]):
+                    # output_image[x+1,y+1,z+1]
+                    if output_image1[x+1,y+1,z+1] != 0:
+                        jug1 = np.amin([output_image1[x, y+1, z+1],output_image1[x+1, y, z+1],output_image1[x+1,y+1,z]])
+                        if output_image1[x+1,y+1,z+1] <= jug1:
+                            output_image1[x+1,y+1,z+1] = output_image1[x+1,y+1,z+1] + 1
+                            Ori = Ori + 1
+                            Jug1 = 1 + Jug1
+
+
+                    x1 = input_dim[0]+2 -x
+                    y1 = input_dim[0]+2 -y
+                    z1 = input_dim[0]+2 -z
+                    if output_image2[x1, y1 ,z1] != 0:
+                        jug2 = np.amin([output_image2[x1+1 , y1, z1],output_image2[x1, y1+1, z1],output_image2[x1, y1, z1+1]]),
+                        if output_image2[x1, y1 ,z1] <= jug2:
+                            output_image2[x1, y1 ,z1] = output_image2[x1, y1 ,z1] + 1
+                            Jug2 = Jug2 + 1
+
+
+
+
+        if ( Ori > Aim and Jug1 == 1 and Jug2 == 1 ):
+            break
+    return output_image1
+
+
+
+
+
+
+    print("the max value of image is " + str(max))
+    print("the min value of image is " + str(min))
+
+    
+
+
+
+    
+    return output_image  #returnsits 3D Euclidean distance transform
+class unknown:
+    def __init__(self) -> None:
+        pass
+
+
+
+Path_of_input = 'label_train00.npy'
+# Path_of_input = 'image_train00.npy'
+
+data = np.load(Path_of_input)
+dim = [20,20,20]
+image = distance_transform_np(data , dim )
+# c = len(image)
+
+plt.imshow(image[:,40,:])
+plt.show()
+
+# print(c)
+
---
+++ cw1/task1/test.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# a = np.ones([2,2])
+# print(a)
+# c = np.zeros([4,4])
+# print(c)
+# c[1:3,1:3] = a
+# print(1:3)
+# b = [2,2]
+# print(b)
+# b = b +1
+# print(b)
+
+
+a = [5 , 2 , 3]
+print(np.amin(a))
