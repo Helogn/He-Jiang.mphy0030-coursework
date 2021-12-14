436f0eef9256510b563a99c3875594a4fba439b9
Helogn <58104248+Helogn@users.noreply.github.com>
2021-12-14 23:57:52 +0000

    change main direction

--- cw1/task1/task.py
+++ cw1/task1/task.py
@@ -34,7 +34,7 @@ import scipy.ndimage as sc
 #     kernal = []
 #     if x,y,z
 
-def distance_transform_np3( input_image , Dis = [1,1,1] ):
+def distance_transform_np( input_image , Dis = [1,1,1] ):
 
     Aim = np.sum(input_image)
     print(" Aim = " + str(Aim))
@@ -72,7 +72,6 @@ def distance_transform_np3( input_image , Dis = [1,1,1] ):
                             output_image[1+x,1+y,1+z] = number
                             media_judge[x+1,y+1,z+1] = 0
 
-        
         judge = media_judge.copy()
         
         SUM_all = np.sum(judge)
@@ -83,20 +82,134 @@ def distance_transform_np3( input_image , Dis = [1,1,1] ):
 
     return output_image[1:input_dim[0],1:input_dim[1],1:input_dim[2]]
 
+def distance_transform_np2( input_image , Dis = [1,1,1] ):
+    
+
+    Aim = np.sum(input_image)
+    print(" Aim = " + str(Aim))
+
+    input_dim = input_image.shape
+
+    # define kernal
+    kernal = np.zeros([3,3,3])
+    kernal[0,1,1] = 1;kernal[1,0,1] = 1;kernal[1,2,1] = 1;kernal[1,1,0] = 1;kernal[1,1,2] = 1;kernal[2,1,1] = 1
+    kernal[1,0,0] = 2;kernal[0,1,0] = 2;kernal[2,1,0] = 2;kernal[1,2,0] = 2;kernal[0,0,1] = 2;kernal[2,0,1] = 2
+    kernal[2,2,1] = 2;kernal[0,2,1] = 2;kernal[1,0,2] = 2;kernal[2,1,2] = 2;kernal[1,2,2] = 2;kernal[0,1,2] = 2
+    kernal[0,0,0] = 3;kernal[2,0,0] = 3;kernal[2,2,0] = 3;kernal[0,2,0] = 3;kernal[0,0,2] = 3;kernal[2,0,2] = 3
+    kernal[2,2,2] = 3;kernal[0,2,2] = 3;kernal[1,1,1] = 4
+
+
+    # expand output_image 2 voxels by each coordinate 
+    output_image = np.zeros([input_dim[0]+2,input_dim[1]+2,input_dim[2]+2])
+    judge = np.zeros([input_dim[0]+2,input_dim[1]+2,input_dim[2]+2])
+    judge[1:input_dim[0]+1,1:input_dim[1]+1,1:input_dim[2]+1] = input_image.copy()
+    media_judge = judge.copy()
+
+    x_ray = np.zeros([input_dim[0]+2,input_dim[1]+2,input_dim[2]+2])
+    y_ray = np.zeros([input_dim[0]+2,input_dim[1]+2,input_dim[2]+2])
+    z_ray = np.zeros([input_dim[0]+2,input_dim[1]+2,input_dim[2]+2])
+
+    SUM_all = np.sum(judge)
+    print(SUM_all)
+    
+    # design matrix
+    
+    while  SUM_all != 0 :
+        
+        for x in range(input_dim[0]):
+            for y in range(input_dim[1]):
+                for z in range(input_dim[2]):
+                    
+                    if judge[x+1,y+1,z+1] != 0:  #change
+
+                        jug_position = judge[x:x+3,y:y+3,z:z+3]*kernal
+
+                        # seek 0 around the central point
+                        jug_min = np.min(jug_position)
+                        
+                        # this point is closed to background
+                        if jug_min == 0:
+
+                            # pos1 = np.where(jug_position == 0)
+                            jug3 = kernal.copy()
+                            jug3[jug_position != 0] = 4
+                            min1 = np.min(jug3)
+                            pos = np.where(jug3 == min1)
+                        
+                            #-------------------------------------------
+                            length_of_arr = np.size(pos,axis = 1)
+                            jug = np.zeros([3,length_of_arr])
+                            jug_value = np.zeros([length_of_arr])
+
+                            for i in range(length_of_arr):
+                                # jug[0][i] = 1
+                                jug[0][i] = abs(pos[0][i]-1) + x_ray[x + pos[0][i],y+pos[1][i],z + pos[2][i]]
+                                jug[1][i] = abs(pos[1][i]-1) + y_ray[x + pos[0][i],y+pos[1][i],z + pos[2][i]]
+                                jug[2][i] = abs(pos[2][i]-1) + z_ray[x + pos[0][i],y+pos[1][i],z + pos[2][i]]
+
+                                jug_value[i] = (jug[0][i])**2 + (jug[1][i])**2 + (jug[2][i])**2
+
+                            max_jug_value = np.min(jug_value)
+                            position_max_jug_value = np.where(jug_value == max_jug_value)
+                            x_ray[x+1,y+1,z+1] = jug[0][position_max_jug_value[0][0]]
+                            y_ray[x+1,y+1,z+1] = jug[1][position_max_jug_value[0][0]]
+                            z_ray[x+1,y+1,z+1] = jug[2][position_max_jug_value[0][0]]
+
+                            output_image[1+x,1+y,1+z] = jug_value[position_max_jug_value[0][0]]**0.5
+                            media_judge[1+x,1+y,1+z] = 0
+
+
+                            #------------------------------------------
+
+
+
+
+                            # # x,y,z_ray save position of each voxel
+                            # x_ray[x+1,y+1,z+1] = abs(pos[0][0]-1) + x_ray[x + pos[0][0],y+pos[1][0],z + pos[2][0]]
+                            # y_ray[x+1,y+1,z+1] = abs(pos[1][0]-1) + y_ray[x + pos[0][0],y+pos[1][0],z + pos[2][0]]
+                            # z_ray[x+1,y+1,z+1] = abs(pos[2][0]-1) + z_ray[x + pos[0][0],y+pos[1][0],z + pos[2][0]]
+                            # # print(x_ray[x+1,y+1,z+1])
+                            
+                            # # calculate distance
+                            # x_near = (x_ray[x+1,y+1,z+1])**2
+                            # y_near = (y_ray[x+1,y+1,z+1])**2
+                            # z_near = (z_ray[x+1,y+1,z+1])**2
+
+                            # # save position
+                            # output_image[1+x,1+y,1+z] = (x_near + y_near+ z_near)**0.5
+                            # media_judge[1+x,1+y,1+z] = 0
+                            # # print((x_near + y_near+ z_near)**0.5)
+
+        judge = media_judge.copy()
+        
+        SUM_all = np.sum(judge)
+
+
+
+        print('SUM_all = ' + str(SUM_all))
+
+    return output_image[1:input_dim[0],1:input_dim[1],1:input_dim[2]]
+
+
+
 Path_of_input = 'label_train00.npy'
 # Path_of_input = 'image_train00.npy'
 
 data = np.load(Path_of_input)
 dim = [20,20,20]
-image = distance_transform_np3(data , dim )
+image = distance_transform_np2(data , dim )
 
 
 
 data = np.load(Path_of_input)
 good = sc.distance_transform_edt(data)
 
-plt.subplot(1, 2, 1)
+plt.subplot(2, 2, 1)
 plt.imshow(image[:,46,:])
-plt.subplot(1, 2, 2)
+plt.subplot(2, 2, 2)
 plt.imshow(good[:,46,:])
+plt.subplot(2, 2, 3)
+plt.imshow(image[:,36,:])
+plt.subplot(2, 2, 4)
+plt.imshow(good[:,36,:])
 plt.show()
--- cw1/task1/test2.py
+++ cw1/task1/test2.py
@@ -13,13 +13,20 @@ import numpy as np
 # kernal[0,1,1] = 1;kernal[1,0,1] = 1;kernal[1,2,1] = 1;kernal[1,1,0] = 1;kernal[1,1,2] = 1;kernal[2,1,1] = 1
 # hh = np.ones([3,3,3])
 # a = np.sum(kernal*hh)
-b = 2
+b = np.ones([3,3,3])
+b[2,2,2] = 5
+b[2,2,1] = 5
 a = b
 
-def jj (t):
+# def jj (t):
+#     t[1] = 3
+#     return t
+kai = np.array([1,2,3,4,5,6,7])
+# h = np.where(kai == 5)
+h = np.zeros([3])
+# jj(a)
 
-    return t+1
+# print(h[2])
 
-jj(a)
-print(b)
-
+# print(np.size(h,axis = 0))
+print(h)
